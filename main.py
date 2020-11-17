f88ac1886cfda31052748bc70dd70f05ad87f5bc
Ryusei2742 <54470490+Ryusei2742@users.noreply.github.com>
2020-11-17 13:38:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ import pandas as pd
 dt_now = str(datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
 
 df = pd.DataFrame(
-    [[dt_now, 'kinocode', 'Hello cron']],
+    [[dt_now, 'test', 'Hello cron']],
     columns = ['datetime', 'name', 'greet'])
 
-df.to_csv('/Users/asd2f/Python/to_excel/' + dt_now + '.csv')
+df.to_csv('場所' + dt_now + '.csv')
 print(dt_now, 'Hello world from cron')
-
-
-# /Library/Frameworks/Python.framework/Versions/3.8/bin:/usr/local/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin
-
 
 
 
